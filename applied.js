ba11f50a1208f3103dcc4160634a4dcaf3c28e32
Arman <92930141+reacharman@users.noreply.github.com>
2024-09-27 11:02:25 +0530

    Update applied.js

--- applied.js
+++ applied.js
@@ -1,8 +1,8 @@
-const appliedJobs = JSON.parse(localStorage.getItem('appliedJobs')) || [];
-const body = document.getElementById('body');
+const appliedJobs = JSON.parse(localStorage.getItem('appliedJobs')) || []
+const body = document.getElementById('body')
 
 appliedJobs.forEach((job, index) => {
-    const row = document.createElement('tr');
+    const row = document.createElement('tr')
 
     row.innerHTML = `
     <td>${job.name}</td>
@@ -10,21 +10,18 @@ appliedJobs.forEach((job, index) => {
     <td>${job.role}</td>
     <td>${job.salary}</td>
     <td><button class="bookmarkBtn" data-index="${index}">Add to Bookmark</button></td>
-    `;
-    body.append(row);
-});
+    `
+    body.append(row)
+})
 
 document.addEventListener('click', function(event) {
-    if (event.target && event.target.classList.contains('bookmarkBtn')) {
-        const jobIndex = event.target.getAttribute('data-index');
-        const jobToBookmark = appliedJobs[jobIndex];
+    if (event.target.classList.contains('bookmarkBtn')) {
+        const jobIndex = event.target.getAttribute('data-index')
+        const jobsToBookmark = appliedJobs[jobIndex]
 
-        let bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
-
-        bookmarks.push(jobToBookmark);
-
-        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-
-        alert('Job added to bookmarks!');
+        let bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || []
+        bookmarks.push(jobsToBookmark)
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+        alert('Bookmark Added!')
     }
-});
+})
